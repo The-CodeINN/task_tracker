f2d8060c68b6009d966d75c16826187923c25491
Richard Emijere <67247138+The-CodeINN@users.noreply.github.com>
2023-03-05 17:14:25 +0100

    Made some changes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       "^/api": {
         // "http://localhost:5000/tasks";
         target:
-          "https://my-json-server.typicode.com/The-CodeINN/task_tracker/tasks",
+          "https://my-json-server.typicode.com/The-CodeINN/task_tracker",
         changeOrigin: true,
         logLevel: "debug",
         pathRewrite: { "^/api": "/" },
